eddfd934fe4509b6479833c8869204409b604a00
BaranBugdaci <baranbgdc44@gmail.com>
2026-07-13 23:14:51 +0300

    Fixing language turkish to english

--- FinalProject_MineSweeper/MineSweeper.java
+++ FinalProject_MineSweeper/MineSweeper.java
@@ -9,28 +9,29 @@ public class MineSweeper {
     int row;
     int column;
     int totalSafeCells;
-    int oppenedCells;
+    int openedCells;
     String[][] board;
     String[][] map;
-
 
     MineSweeper() {
     }
 
+    // Orchestrates the entire game flow
     void run(){
-        matrisCount();
-        preaperMap();
+        setMatrixSize();
+        prepareMap();
         System.out.println("========================");
 
         play();
     }
 
-    void matrisCount() {
+    // Gets game board dimensions from the user
+    void setMatrixSize() {
         while(true) {
-            System.out.println("Lütfen Oyun tahtasının boyutunu ayarlayınız ");
-            System.out.print("Satır Sayısını Giriniz(Minimum 2): ");
+            System.out.println("Please configure the game board size:");
+            System.out.print("Enter Number of Rows (Minimum 2): ");
             this.row = scanner.nextInt();
-            System.out.print("Sütun Sayısını Giriniz(Minimum 2): ");
+            System.out.print("Enter Number of Columns (Minimum 2): ");
             this.column = scanner.nextInt();
 
             if(this.row >= 2 && this.column >= 2) {
@@ -38,12 +39,12 @@ public class MineSweeper {
                 this.map = new String[this.row][this.column];
                 break;
             }
-            System.out.println("HATA! Matris Boyutu Minimum 2x2 Olabilir\n");
+            System.out.println("ERROR! Board size must be at least 2x2.\n");
         }
     }
 
-    void preaperMap() {
-        // İki tahtayı da "-" değeri ile dolduruyoruz
+    void prepareMap() {
+        // Fills both boards with the empty symbol "-"
         for(int i = 0; i < this.row; i++){
             for(int j = 0; j < this.column; j++){
                 this.board[i][j] = this.empty;
@@ -51,15 +52,17 @@ public class MineSweeper {
             }
         }
 
-        // Mayınları yerleştirme
+        // Setting up the mines
         int mineCount = (row * column) / 4;
         int placedMine = 0;
         this.totalSafeCells = (row * column) - mineCount;
 
+        // Places mines at random coordinates
         while(placedMine < mineCount){
             int randomRow = (int)(Math.random() * row);
             int randomColumn = (int)(Math.random() * column);
 
+            // Checks if there is already a mine in the map before placing one
             if(!this.map[randomRow][randomColumn].equals("*") && (!this.board[randomRow][randomColumn].equals("*"))){
                 this.map[randomRow][randomColumn] = "*";
                 placedMine++;
@@ -67,32 +70,33 @@ public class MineSweeper {
         }
     }
 
-
+    // Main game mechanics loop
     void play() {
         while(true) {
             print();
 
-            System.out.print("Satır sırasını giriniz: ");
+            System.out.print("Enter row index: ");
             int chosenRow = scanner.nextInt();
-            System.out.print("Sütun sırasını giriniz: ");
+            System.out.print("Enter column index: ");
             int chosenColumn = scanner.nextInt();
 
             if (chosenRow < 0 || chosenRow >= this.row || chosenColumn < 0 || chosenColumn >= this.column){
-                System.out.println("HATA! Girdiğiniz koordinatlar harita sınırları dışında.");
+                System.out.println("ERROR! Chosen coordinates are out of bounds.");
                 continue;
             }
 
             if (!this.board[chosenRow][chosenColumn].equals(this.empty)){
-                System.out.println("Girdiğiniz Koordinata daha önce işlem yaptınız. Başka bir kordinat giriniz!");
+                System.out.println("You have already selected these coordinates. Please try another one!");
                 continue;
             }
 
+            // Lose scenario
             if(this.map[chosenRow][chosenColumn].equals(this.mine)){
                 System.out.println("BOOM! Game Over!");
 
+                // Reveals all mine locations upon losing
                 for (int i = 0; i < this.row; i++) {
                     for (int j = 0; j < this.column; j++) {
-                        //mayınların nerde olduğunu gösterir
                         if (this.map[i][j].equals(this.mine)) {
                             this.board[i][j] = this.mine;
                         }
@@ -100,19 +104,18 @@ public class MineSweeper {
                 }
 
                 print();
-
                 break;
             }
 
             int mineCount = 0;
 
-            for (int i = -1; i <= 1;i++){
-                for(int j = -1; j <= 1;j++){
+            // Iterates through neighboring cells to count adjacent mines
+            for (int i = -1; i <= 1; i++){
+                for(int j = -1; j <= 1; j++){
                     int neighborRow = chosenRow + i;
                     int neighborColumn = chosenColumn + j;
 
                     if((neighborRow >= 0 && neighborRow < this.row) && (neighborColumn >= 0 && neighborColumn < this.column)){
-
                         if(this.map[neighborRow][neighborColumn].equals(this.mine)){
                             mineCount++;
                         }
@@ -121,25 +124,26 @@ public class MineSweeper {
             }
             this.board[chosenRow][chosenColumn] = String.valueOf(mineCount);
 
-            this.oppenedCells++;
+            this.openedCells++;
 
-            if(this.totalSafeCells == this.oppenedCells){
+            // Win scenario
+            if(this.totalSafeCells == this.openedCells){
                 print();
-                System.out.println("Congrulations! You Won!");
+                System.out.println("Congratulations! You Won!");
                 break;
             }
-
         }
     }
+
+    // Prints the current state of the board to the console
     void print(){
         System.out.println("===================================");
-        for (int i = 0; i < this.row;i++){
-            for(int j = 0; j < this.column;j++){
+        for (int i = 0; i < this.row; i++){
+            for(int j = 0; j < this.column; j++){
                 System.out.print(this.board[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("===========================");
+        System.out.println("===================================");
     }
-
 }
